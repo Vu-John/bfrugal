cd7541477a82d3164ee51effc359ca9868e05339
Vu-John <johnvu1995@gmail.com>
2018-12-27 21:45:31 -0500

    Setup redux, added login component, installed bootstrap, clean up css files

---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,8 @@
+import { connect } from "react-redux";
+import Login from "../components/Login";
+// import { addTodo } from "../actions";
+
+export default connect(
+  null
+  //   { addTodo }
+)(Login);
