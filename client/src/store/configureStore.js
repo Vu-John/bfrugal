3946cd1b53ea95b8ae256dc1e6b4b90e595f8424
Vu-John <johnvu1995@gmail.com>
2019-01-07 03:51:54 -0500

    Remove redux-logger for prod

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,13 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { createLogger } from "redux-logger";
 import rootReducer from "../reducers";
 
-const loggerMiddleware = createLogger();
-
-const configureStore = createStore(
-  rootReducer,
-  applyMiddleware(thunk, loggerMiddleware)
-);
+const configureStore = createStore(rootReducer, applyMiddleware(thunk));
 
 export default configureStore;
